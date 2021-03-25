9c2642764512e95d3c50bd45de1fff0c2691bb0d
Note-HST <jorge_mira@hst.com.br>
2021-03-25 01:39:00 -0300

    Incluindo .js

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector(".titulo-barra-superior");			
+titulo.textContent = "Hotel Orion - Hortolândia"
+console.log(titulo);
+console.log(titulo.textContent);
+			
+//console.log("Hello World - Console")
+//alert("Ola, Bem Vindo(a) ao Hotel Orion")
+//console.log(document);
