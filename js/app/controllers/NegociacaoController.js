dab7d9e39a539ad520cd8bd859f476265faf6f82
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 19:39:33 -0300

    Error data.getDate

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -3,16 +3,28 @@ class NegociacaoController{
     constructor(){
 
         let $ = document.querySelector.bind(document); 
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         }
         
         adiciona(event){
             event.preventDefault();        
             
-            console.log(this.inputData.value);
-            console.log(this.inputQuantidade.value);
-            console.log(this.inputValor.value);
+            console.log(typeof(this._inputData.value));//VERIFICANDO QUE TIPO ESTOU RECEBENDO
+            console.log(this._inputData.value);//VERIFICANDO O VALOR EXATO DA STRING
+            
+            //RETORNANDO ERRO NA RECUPERAÇÃO DA DATA
+            let negociacao = new Negociacao(
+                this._inputData.value, 
+                this._inputQuantidade.value,
+                this._inputValor.value
+            );
+                console.log(negociacao);
+            //adicionando a negociacao em uma lista
+
+            //console.log(this._inputData.value);
+            //console.log(this._inputQuantidade.value);
+            //console.log(this._inputValor.value);
     }
 }
