4a4188da2c708ba0edc1e37bfc3c8c04d6fa7cf4
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 20:35:01 -0300

    Criando Objeto Date

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,20 +11,9 @@ class NegociacaoController{
         adiciona(event){
             event.preventDefault();        
             
-            console.log(typeof(this._inputData.value));//VERIFICANDO QUE TIPO ESTOU RECEBENDO
-            console.log(this._inputData.value);//VERIFICANDO O VALOR EXATO DA STRING
-            
-            //RETORNANDO ERRO NA RECUPERAÇÃO DA DATA
-            let negociacao = new Negociacao(
-                this._inputData.value, 
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
-                console.log(negociacao);
-            //adicionando a negociacao em uma lista
+            //2021-03-21    
+            let data = new Date(this._inputData.value.replace(/-/g, ','));
 
-            //console.log(this._inputData.value);
-            //console.log(this._inputQuantidade.value);
-            //console.log(this._inputValor.value);
+            console.log(data);            
     }
 }
