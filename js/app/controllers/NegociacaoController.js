710463801735065dd4de350ff629af290234bcaf
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 18:41:58 -0300

    Associando metodos/bind(document)

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -2,6 +2,16 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        alert('Chamei ação no controller');
+
+
+        let $ = document.querySelector.bind(document);//ASSOCIAÇÃO COM O $ AO QUERY COM O IND MATEM O DOCUMENT
+        
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+        
+        console.log(data.value);
+        console.log(quantidade.value);
+        console.log(valor.value);
     }
 }
