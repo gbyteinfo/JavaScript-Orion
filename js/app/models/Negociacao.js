0a2a512efc92c0fa5bab81de5c36a13e477edd37
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 00:09:42 -0300

    Criando Getters

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -8,16 +8,16 @@ class Negociacao {
         this._valor = valor;
     }
     //ENCAPSULAMENTO E METODOS DE LEITURA
-    getVolume(){
+    get volume(){
         return this._quantidade * this._valor;
     }
-    getData(){
+    get data(){
         return this._data;
     }
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     }
-    getValor(){
+    get valor(){
         return this._valor;
     }
 }
