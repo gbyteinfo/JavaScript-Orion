1b6409a3c7e73c7212bea456634655fcb42cde9b
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 19:13:31 -0300

    Criando constructor do Controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,25 +1,18 @@
 class NegociacaoController{
 
-    adiciona(event){
-        event.preventDefault();
+    constructor(){
 
-        //VARIAAVEL $ RECEBE O QUERY SELECTOR DO DOCUMENT
-        //COM O .BIND EU INCLUO NOVAMENTE O QUERY SELECTOR
-        //PARA O CONTEXTO DO DOCUMENT NOVAMENTE, AGORA REPRESENTADO 
-        //PELO $ 
-        let $ = document.querySelector.bind(document);
-
-        //ASSIM O QUERY SELECTOR ESTA SENDO EXECUTADO 
-        //FORA DO CONTEXTO DOCUMENT, A QUAL A FUNÇÃO 
-        //PERTENCE E TEM CHAMADA PARA O THIS 
-        //let $ = document.querySelector; 
+        let $ = document.querySelector.bind(document); 
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+        }
         
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
-        
-        console.log(data.value);
-        console.log(quantidade.value);
-        console.log(valor.value);
+        adiciona(event){
+            event.preventDefault();        
+            
+            console.log(this.inputData.value);
+            console.log(this.inputQuantidade.value);
+            console.log(this.inputValor.value);
     }
 }
