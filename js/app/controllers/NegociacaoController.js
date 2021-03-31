74e70f6e0230022d3f87b7e1d69f23289b037167
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 18:58:10 -0300

    Comment in .bind(document)

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -3,8 +3,16 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
+        //VARIAAVEL $ RECEBE O QUERY SELECTOR DO DOCUMENT
+        //COM O .BIND EU INCLUO NOVAMENTE O QUERY SELECTOR
+        //PARA O CONTEXTO DO DOCUMENT NOVAMENTE, AGORA REPRESENTADO 
+        //PELO $ 
+        let $ = document.querySelector.bind(document);
 
-        let $ = document.querySelector.bind(document);//ASSOCIAÇÃO COM O $ AO QUERY COM O IND MATEM O DOCUMENT
+        //ASSIM O QUERY SELECTOR ESTA SENDO EXECUTADO 
+        //FORA DO CONTEXTO DOCUMENT, A QUAL A FUNÇÃO 
+        //PERTENCE E TEM CHAMADA PARA O THIS 
+        //let $ = document.querySelector; 
         
         let inputData = $('#data');
         let inputQuantidade = $('#quantidade');
