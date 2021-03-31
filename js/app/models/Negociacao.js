1878fb78801ed59be13947701c3a75df5f8edf7b
NOTE-HST <jorge_mira@hst.com.br>
2021-03-30 23:38:33 -0300

    Encapsulamento e metodos de leitura

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -3,11 +3,21 @@
 //PARA A APLICAÇÃO, CLIENADO MINHA PRIMEIRA CLASSE DA APP 
 class Negociacao {
     constructor(data, quantidade,valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
-    obtemVolume(){
-        return this.quantidade * this.valor;
+    //ENCAPSULAMENTO E METODOS DE LEITURA
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+    getData(){
+        return this._data;
+    }
+    getQuantidade(){
+        return this._quantidade;
+    }
+    getValor(){
+        return this._valor;
     }
 }
