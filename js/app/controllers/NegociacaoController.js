a1d056f9b48c9dd0074ba459ca9f712d7398c080
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 21:40:11 -0300

    Solução Data(item[mes - 1])

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,8 +11,23 @@ class NegociacaoController{
         adiciona(event){
             event.preventDefault();        
             
-            //2021-03-21    
-            let data = new Date(this._inputData.value.replace(/-/g, ','));//tratando o recebimento data
+            //RECEBENDO ANO MES E DIA SEPARADAMENTE
+            
+            //SPREAD OPERATOR
+            // USO DO ... DIZ QUE O ARRAY PASSADO PRO DATE TEM QUE SER DESMEMBRADO
+            // O PRIMEIRO ITEM DO ARRAY É O PRIMEIRO PARAMENTRO DO CONSTRUTO DO DATE(nesse exemplo)
+            
+            let data = new Date(...
+                this._inputData.value
+                .split('-')//SPLIT CRIA O ARRAY DO VALOR DATA, "¹ITEM MES INICIA NO 0
+                .map(function(item, indice) {//VARRE TODO O ARRAY
+                    if(indice == 1){
+                        return item - 1; //"¹DECREMENTO O MES PARA EXECUTAR O CORRETO
+                    }else {
+                        return item;
+                    }
+                })
+            );//tratando o recebimento data
 
             console.log(data);//bjeto data configurado para ser tratado          
     }
