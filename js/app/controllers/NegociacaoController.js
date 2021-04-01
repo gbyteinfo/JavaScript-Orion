f771220ef3ee9c8dbc4d0f8fa28f31ce331f5684
NOTE-HST <jorge_mira@hst.com.br>
2021-03-31 22:16:39 -0300

    Ajustando com Arrow Function

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -20,9 +20,10 @@ class NegociacaoController{
             let data = new Date(...
                 this._inputData.value
                 .split('-')//SPLIT CRIA O ARRAY DO VALOR DATA, "¹ITEM MES INICIA NO 0
-                .map(function(item, indice) {//VARRE TODO O ARRAY ITEM POR ITEM
-                    return item - indice % 2;//"¹DECREMENTO O MES PARA EXECUTAR O CORRETO
-                })
+                .map((item, indice) => item - indice % 2)// ARROW FUNCTION OMITO O FUNCTION COM =>
+                //VARRE TODO O ARRAY ITEM POR ITEM 
+                //"¹DECREMENTO O MES PARA EXECUTAR O CORRETO
+
             );//tratando o recebimento data
 
             console.log(data);//bjeto data configurado para ser tratado          
