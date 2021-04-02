01cac15efb4162923f1c09cbdaa14dae3e67c4d7
NOTE-HST <jorge_mira@hst.com.br>
2021-04-02 11:19:52 -0300

    Capturando dados no metodo adiciona

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,26 +4,32 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        console.log("Data dentro do Constructor: " + this._inputData.value);
+        //console.log("Data dentro do Constructor: " + this._inputData.value);
         };
         adiciona(event){
             event.preventDefault();
 
-            console.log("Data no Metodo Adiciona: " + this._inputData.value);
+            //console.log("Data no Metodo Adiciona: " + this._inputData.value);
             let data = new Date(
                 ...this._inputData.value
                 .split('-')
                 .map((item, indice) => item - indice % 2)
             );
-            console.log("Data no Metodo Adiciona depois: " + this._inputData.value);
+            //console.log("Data no Metodo Adiciona depois: " + this._inputData.value);
             
             let negociacao = new Negociacao(
                 data,
                 this._inputQuantidade.value,
                 this._inputValor.value
             );
+            let diaMesAno = negociacao.data.getDate() + ' / ' 
+                          + (negociacao.data.getMonth() + 1) + ' / ' //CONTADOR DO MES DENTRO DO ARRAY INICIA NO 0
+                          + negociacao.data.getFullYear() ; 
 
-            console.log("Data depois da classe negociacao: " + negociacao.data);
+
+            console.log(diaMesAno);
+            console.log(data);
+            //console.log("Data depois da classe negociacao: " + negociacao.data);
 
         };
 }
