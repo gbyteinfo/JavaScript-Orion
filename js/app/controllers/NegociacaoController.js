ffa48d1a4d5a90f4fc641232fb70fc4ca78ca4a5
NOTE-HST <jorge_mira@hst.com.br>
2021-04-02 16:41:35 -0300

    Criando Alert em adiciona(event)

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -9,14 +9,40 @@ class NegociacaoController{
         adiciona(event){
             event.preventDefault();
 
+            // 1 CRIA NEGOCIÇÃO
             let negociacao = new Negociacao(
                 //DATEHELPER SENDO CHAMADA NA CLASSE, METODOS DEFINIDOS COMO STATICOS
                 DateHelper.textoParaData(this._inputData.value),//USANDO DIRETAMENTE O RETORNO DATA
                 this._inputQuantidade.value,
                 this._inputValor.value
             );
-
+                
+            //ADICIONA NA LISTA
             this._listaNegociacoes.adiciona(negociacao);
+            alert(`
+                ADICIONADO: \n
+                Data:............... ${DateHelper.dataParaTexto(negociacao.data)}
+                Quantidade:.. ${this._inputQuantidade.value}
+                Valor:.............. ${this._inputValor.value} 
+            `);
+            //LIMPA A LISTA
+
             console.log(this._listaNegociacoes.Negociacoes);
+            this._limpaFormulario();
         };
+
+        //metodo so pode ser chamado pela classe
+        //METODO SÓ PODE SER CHAMADO PELA CLASSE
+        _limpaFormulario(){
+            this._inputData.value = '';
+            this._inputQuantidade.value = 1;
+            this._inputValor.value = 0.0;
+
+            //DEFNINDO O FOCUS (CURSOR DEPOIS DA OPERACAO)
+            this._inputData.focus();
+            //this._inputQuantidade.focus();
+            //this._inputValor.focus();
+            
+            
+        }
 }
