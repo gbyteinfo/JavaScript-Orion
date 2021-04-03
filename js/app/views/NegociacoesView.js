670f324aa3c99e02a73f6dc532d3a812d2e49669
NOTE-HST <jorge_mira@hst.com.br>
2021-04-02 22:49:32 -0300

    NegociacoesView.js e Passando Table para class

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,18 +1,20 @@
-/*
-<table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-        </tbody>
-        
-        <tfoot>
-        </tfoot>
-    </table>
-*/
+class NegociacoesView{
+    template(){
+        `<table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+            </tbody>
+            
+            <tfoot>
+            </tfoot>
+        </table>`
+    }
+}
