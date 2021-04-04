ea421923f369ad7c3569f80478d90b2e93987ab6
NOTE-HST <jorge_mira@hst.com.br>
2021-04-03 22:36:00 -0300

    Criando exption error em view.js

--- js/app/views/View.js
+++ js/app/views/View.js
@@ -2,7 +2,9 @@ class View{
     constructor(elemento){
         this._elemento = elemento;
     }
-     _template(model){}
+     _template(model){
+         throw new Error(`Método update tem que ser implementado`);
+     }
 
      update(model){
         this._elemento.innerHTML = this._template(model);
