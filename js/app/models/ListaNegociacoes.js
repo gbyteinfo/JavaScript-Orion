32d958a368088befd4a6abb7d90f59f3107d6b54
NOTE-HST <jorge_mira@hst.com.br>
2021-04-04 20:28:42 -0300

    Armadilhas com Reflect.get Completo

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,26 +1,22 @@
 class ListaNegociacoes{
-    constructor(armadilha){ //indico que ninguem adicona fora da classe
+    constructor(){ //indico que ninguem adicona fora da classe
         this._negociacoes = [];
-        this._armadilha = armadilha;
+
     };
 
-    //adiciono o unico metodo para incluir negociacoes 
+
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        //Reflect.apply(this._armadilha,this._contexto, [this])//é uma classe que chama o metodo statico dessa classe
-        //Reflect .aply recebe como parametro afução que qeuro executar
-        //o contexto que quero executar
-        // e os parametros que serao passado para a funcao no caso em array
+
     };
     get negociacoes(){
-        //BLINDANDO O METODO CRIANDO UM ARRAY VAZIO ANTES DE CRIAR APENAS 1 CORRETO(COMO ZERARO ARRAY ANTERIOR)
-        return [].concat(this._negociacoes);//CRIA UM NOVO ARRAY CO METODO NEGOCIAÇÃO
+       
+        return [].concat(this._negociacoes);
         
     };
     esvazia(){
         this._negociacoes = [];
-        this._armadilha(this);
+
         
     }
 }
