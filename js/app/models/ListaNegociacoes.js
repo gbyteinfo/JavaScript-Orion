a8cf809281aa3d59086a9587a0d86ececd5db4ad
NOTE-HST <jorge_mira@hst.com.br>
2021-04-04 19:42:48 -0300

    Função lexiaEscopo lexico

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,15 +1,14 @@
 class ListaNegociacoes{
-    constructor(contexto, armadilha){ //indico que ninguem adicona fora da classe
+    constructor(armadilha){ //indico que ninguem adicona fora da classe
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     };
 
     //adiciono o unico metodo para incluir negociacoes 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha,this._contexto, [this])//é uma classe que chama o metodo statico dessa classe
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha,this._contexto, [this])//é uma classe que chama o metodo statico dessa classe
         //Reflect .aply recebe como parametro afução que qeuro executar
         //o contexto que quero executar
         // e os parametros que serao passado para a funcao no caso em array
@@ -21,7 +20,7 @@ class ListaNegociacoes{
     };
     esvazia(){
         this._negociacoes = [];
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha,this._contexto, [this])
+        this._armadilha(this);
+        
     }
 }
